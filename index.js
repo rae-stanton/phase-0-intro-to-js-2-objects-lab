208677631677250d9ee0046fb95d30385ec9acf1
Rae Stanton <raestanton93@gmail.com>
2023-07-13 19:27:18 -0700

    Objects lab
    For the JS objects lab.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+  luna: 'ninja',
+  faust: 'food critic',
+  simon: 'zookeeper',
+  kayla: 'queen of england',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
